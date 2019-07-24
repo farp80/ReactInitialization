82b1bd5795f305fbb9a8cd43eef700e71b75b837
farp80 <farp80@gmail.com>
2019-07-24 00:45:15 +0000

    ended 1.7 exercise

--- exercises/01.5-building-html-from-arrays/index.js
+++ exercises/01.5-building-html-from-arrays/index.js
@@ -7,3 +7,29 @@ const navlinkItems = [];
 const content = <ul className="nav">{navlinkItems}</ul>;
 
 ReactDOM.render(content, document.querySelector("#myDiv"));
+
+let links = [
+	{
+		link: "http://www.google.com",
+		text: "Link to google.com"
+	},
+	{
+		link: "http://www.facebook.com",
+		text: "Link to facebook.com"
+	},
+	{
+		link: "http://www.amazon.com",
+		text: "Link to amazon.com"
+	}
+];
+
+const listItems = links.map((value, index) => (
+	<li key={index.toString()} className="nav-item">
+		<a className="nav-link" href={value.link}>
+			{value.text}
+		</a>
+	</li>
+));
+
+const unsortedList = <ul>{listItems}</ul>;
+ReactDOM.render(unsortedList, document.querySelector("#myDiv"));
